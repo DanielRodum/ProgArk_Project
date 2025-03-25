59f69b82315fe22639bc9d8ffb21a65cf6e4d04d
Adrian Hovatn Marstokk <adrianhm@stud.ntnu.no>
2025-03-25 18:51:13 +0100

    #3 - Create lobby view implimented, still needs work to allow actual players to join and to select actual wordsets.

---
+++ core/src/main/java/com/mygdx/game/controller/CreateLobbyController.java
@@ -0,0 +1,61 @@
+package com.mygdx.game.controller;
+
+import com.mygdx.game.DoodleWars;
+import com.mygdx.game.view.CreateLobbyView;
+
+import java.util.Random;
+
+public class CreateLobbyController {
+    private DoodleWars game;
+    private CreateLobbyView view;
+
+    public CreateLobbyController(DoodleWars game, CreateLobbyView view) {
+        this.game = game;
+        this.view = view;
+    }
+
+    /**
+     * Generate a random lobby code
+     * 
+     * @return A random alphanumeric code
+     */
+    public static String generateLobbyCode() {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder code = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 6; i++) {
+            code.append(chars.charAt(random.nextInt(chars.length())));
+        }
+
+        return code.toString();
+    }
+
+    /**
+     * Handle player joining the lobby
+     * 
+     * @param playerName Name of the player joining
+     */
+    public void handlePlayerJoin(String playerName) {
+        view.addPlayer(playerName);
+        System.out.println("Player joined: " + playerName);
+    }
+
+    /**
+     * Handle player leaving the lobby
+     * 
+     * @param playerName Name of the player leaving
+     */
+    public void handlePlayerLeave(String playerName) {
+        view.removePlayer(playerName);
+        System.out.println("Player left: " + playerName);
+    }
+
+    /**
+     * Handle starting the game
+     */
+    public void handleStartGame() {
+        System.out.println("Game starting!");
+        // TODO: Implement game start logic
+    }
+}
--- core/src/main/java/com/mygdx/game/controller/MainMenuController.java
+++ core/src/main/java/com/mygdx/game/controller/MainMenuController.java
@@ -1,29 +1,31 @@
 package com.mygdx.game.controller;
 
 import com.mygdx.game.DoodleWars;
+import com.mygdx.game.view.CreateLobbyView;
 import com.mygdx.game.view.MainMenuView;
 
 public class MainMenuController {
     private DoodleWars game;
     private MainMenuView view;
 
-    public MainMenuController(DoodleWars game, MainMenuView view){
+    public MainMenuController(DoodleWars game, MainMenuView view) {
         this.game = game;
         this.view = view;
     }
 
-    public void handleCreateLobby(){
+    public void handleCreateLobby() {
         System.out.println("Create Lobby Clicked!");
-        //TODO: implement logic
+        String lobbyCode = CreateLobbyController.generateLobbyCode();
+        game.setScreen(new CreateLobbyView(game, lobbyCode));
     }
 
-    public void handleJoinLobby(){
+    public void handleJoinLobby() {
         System.out.println("Join Lobby Clicked!");
-        //TODO: implement logic
+        // TODO: implement logic
     }
 
-    public void handleTutorial(){
+    public void handleTutorial() {
         System.out.println("Tutorial Clicked!");
-        //TODO: implement logic
+        // TODO: implement logic
     }
 }
